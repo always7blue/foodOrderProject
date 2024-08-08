4da228062352112df1cbf7ec03d767ab3afd31fb
always7blue <cansu7sahin@gmail.com>
2024-08-08 10:50:04 +0300

    added somethin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FoodPageComponent } from './food-page/food-page.component';
+// Uygulamanızın yönlendirme (routing) yapılandırmasını tanımlayın
+const routes: Routes = [
+  // Boş ('') yolun, HomeComponent bileşenini göstereceğini belirtir
+  { path: '', component: HomeComponent },
+  // 'search/:searchTerm' yolun, HomeComponent bileşenini göstereceğini ve searchTerm parametresini alacağını belirtir
+  { path: 'search/:searchTerm', component: HomeComponent },
 
-const routes: Routes = [];
+  { path: 'tag/:tag', component: HomeComponent},
 
+  { path: 'food/:id', component: FoodPageComponent}
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
